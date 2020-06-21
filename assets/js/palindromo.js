7715b0c139fdc8d6b57f4746949403e57ebf64ac
luiscolungaperez <perez.luis.colunga@gmail.com>
2020-06-21 18:51:17 -0500

    Agregando un script con html

---
+++ assets/js/palindromo.js
@@ -0,0 +1,37 @@
+const formatWord = (word) => {
+  let newWord = "";
+  const wordLower = word.toLowerCase();
+  for (let i = 0; i < wordLower.length; i++) {
+    if (wordLower[i] !== " ") {
+      newWord = newWord + wordLower[i];
+    }
+  }
+  return newWord;
+};
+
+const isPalindrome = (word) => {
+  const newWord = formatWord(word);
+
+  i = 0;
+  j = newWord.length - 1;
+
+  while (newWord[i] == newWord[j]) {
+    if (i === j) {
+      return "Es palindromo";
+      break;
+    }
+    i++;
+    j--;
+  }
+
+  if (newWord[i] !== newWord[j]) {
+    return "No es palindromo";
+  }
+};
+
+const execute = () => {
+  words = document.getElementById("word").value;
+  document.getElementById("content").innerHTML = words;
+  document.getElementById("result").innerHTML = isPalindrome(words);
+  document.getElementById("word").value = "";
+};
